b246e9b49474b7248318364de24e6dc6d9df98cc
MariamElsoufyy <mariamelsoufyx@gmail.com>
2024-12-13 16:48:13 +0200

    yarab

--- process_generator.c
+++ process_generator.c
@@ -1,78 +1,146 @@
-#include "headers.h"
 
+#include "queue.c"
+/////functions Definitions
 void clearResources(int);
-// Define the Process_Data struct
-typedef struct Process_Data {
-    int id;
-    int arrival;
-    int runtime;
-    int priority;
-} Process_Data;
-// Define the Queue structure
-typedef struct Queue
- {
-    Process_Data *data;      // Array of Process_Data structs
-    int front;
-    int rear;
-    int capacity;
-    int size;
-    
-} Queue;
 
+int forkScheduler(int schedulingalgo, int quantum, int ProcessCount);
+int forkClk();
+void forkClockScheduler(char *algorthmNo, char *quantum, char *countProcesses);
 
+/// global variables ///
+int msgqID;
+pid_t clkpid, schedpid;
+Process_Data *Queuepointer;
+Process_Data *PCB;
 
+// argv[0] useless //argv[1] filename //argv[2] sched algo //argv[3] useless  //argv[4n  ] useless //argv[5] round robin Quantum
+// process_generator.o testcase.txt -sch 5 -q 2
+//    0                   1           2  3 4  5
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
-    // TODO Initialization
-    // 1. Read the input files.
-    FILE *processes_file = fopen("testcase.txt", "r");
-    if (processes_file == NULL) 
+
+    char *filename = argv[1];
+    int schedulingalgorithm = atoi(argv[3]);
+    int quantum = atoi(argv[5]);
+
+    key_t key = ftok("schedulerqueue", 'S');
+    msgqID = msgget(key, IPC_CREAT | 0666);
+
+    FILE *processes_file = fopen(filename, "r");
+    if (filename == NULL)
     {
         perror("Error in opening the file!");
         exit(-1);
     }
-    int processes_number=0;
+
+    Queue *processQueue = createQueue();
+    int procid, arrtime, runtime, priority, processcount = 0;
     char line[100];
-    while (fgets(line, sizeof(line), processes_file) != NULL) 
+
+    Process_Data *procpointer;
+    while (fgets(line, sizeof(line), processes_file) != NULL)
     {
-        if (line[0] != '#')        //first character in line is # so skip comment
+        printf("ya mosahel yarab");
+        if (line[0] != '#') // first character in line is # so skip comment
         {
-            processes_number++;   //to count number of processes in the file
+            sscanf(line, "%d\t%d\t%d\t%d", &procid, &arrtime, &runtime, &priority);
+            processcount++; // to count number of processes in the file
+
+            Process_Data proc;
+            proc.id = procid;
+            proc.arrival = arrtime;
+            proc.runtime = runtime;
+            proc.priority = priority;
+            enqueue(processQueue, proc);
         }
     }
     fclose(processes_file);
-    processes_file = fopen("testcase.txt", "r");
-    char str[100];
-    printf("number of processes = %d\n", processes_number);
-    fgets(str, 100, processes_file); // skips first line
-    for (int i = 0; i < processes_number - 1; i++) //creates a process object for each process
-    {
-        struct Process_Data Temp_Process;
-        fscanf(processes_file, "%d %d %d %d", &Temp_Process.id, &Temp_Process.arrival, &Temp_Process.runtime, &Temp_Process.priority);
-        // Temp_Process.remTime=Temp_Process.runningtime;
-        // Temp_Process.state=Stopped;
-        // Temp_Process.waitingTime=0;
-        // Temp_Process.idTextFile=newProcess.id;
-        // enqueue(newProcess, inputProcesses, lines);
-    }
-    // fadl bas a enqueue kol mara al struct dah gwa al queue bt3tyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
-    
-    // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
-    // 3. Initiate and create the scheduler and clock processes.
-    // 4. Use this function after creating the clock process to initialize clock.
-    initClk();
-    // To get time use this function. 
-    int x = getClk();
-    printf("Current Time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
-    destroyClk(true);
+
+    displayQueue(processQueue);
+
+    return 0;
+    printf("%d \n ", processcount);
+    char c[100];
+    snprintf(c, sizeof(c), "%d", processcount);
+    forkClk();
+
+    kill(getppid(), SIGINT);
 }
+
+////////////////////////////////////functions////
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    // TODO Clears all resources in case of interruption
+    kill(clkpid, SIGINT);
+    kill(schedpid, SIGINT);
+    /// free(Queuepointer);
+    msgctl(msgqID, IPC_RMID, (struct msqid_ds *)0);
+    exit(0);
+}
+
+///////////////////////////////////////////forking functions////
+
+int forkClk()
+{
+
+    int clk_pid = fork();
+    printf("%d \n ", clk_pid);
+    if (clk_pid == -1)
+    {
+        perror("Error in forking clock");
+        exit(-1);
+    }
+    else if (clk_pid == 0)
+    {
+        execl("./clk.out", "clk.out", NULL);
+        sleep(3);
+    }
+    return clk_pid;
+}
+
+int forkScheduler(int schedulingalgo, int quantum, int ProcessCount)
+{
+
+    int scheduler_pid = fork();
+    if (scheduler_pid == -1)
+    {
+        perror("Error in fork");
+        exit(-1);
+    }
+    else if (scheduler_pid == 0)
+    {
+        execl("./scheduler.out", "scheduler.out", schedulingalgo, quantum, ProcessCount, NULL);
+    }
+
+    return scheduler_pid;
+}
+
+void forkClockScheduler(char *algorthmNo, char *quantum, char *countProcesses)
+{
+    int clk_pid = fork();
+    if (clk_pid == -1)
+    {
+        perror("Error in fork");
+        exit(-1);
+    }
+    else if (clk_pid == 0)
+    {
+        execl("./clk.out", "clk.out", NULL);
+        sleep(3);
+    }
+    else
+    {
+        int scheduler_pid = fork();
+        if (scheduler_pid == -1)
+        {
+            perror("Error in fork");
+            exit(-1);
+        }
+        else if (scheduler_pid == 0)
+        {
+            execl("./scheduler.out", "scheduler.out", algorthmNo, quantum, countProcesses, NULL);
+        }
+    }
 }
---
+++ queue.c
@@ -0,0 +1,102 @@
+
+#include "structs.c"
+#include <stdio.h>
+
+
+void displayProcessData(Process_Data data){
+    
+    printf("#id arrival runtime priority\n");
+    printf("%d\t%d\t%d\t%d\n",data.id,data.arrival,data.runtime,data.priority);
+}
+
+typedef struct Node {
+    Process_Data data;
+    struct Node* next;
+} Node;
+
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+
+
+Node* createNode(Process_Data data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+Queue* createQueue() {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    if (queue==NULL) {
+        printf("Error in creating Queue\n");
+        return NULL;
+    }
+    queue->front = queue->rear = NULL;
+    return queue;
+}
+
+int isEmpty(Queue* queue) {
+    return queue->front == NULL;
+}
+
+// Function to add an element to the queue
+void enqueue(Queue* queue, Process_Data data) {
+    Node* newNode = createNode(data);
+    if (queue->rear == NULL) {
+        queue->front = queue->rear = newNode;
+        return;
+    }
+    queue->rear->next = newNode;
+    queue->rear = newNode;
+}
+
+void dequeue(Queue* queue,Process_Data* proc) {
+    if (isEmpty(queue)) {
+        printf("Queue is empty, cannot dequeue\n");
+        return;
+    }
+    Node* temp = queue->front;
+    Process_Data data = temp->data;
+    queue->front = queue->front->next;
+
+    if (queue->front == NULL) {
+        queue->rear = NULL;
+    }
+
+    free(temp);
+    proc = &data;
+}
+
+
+void peek(Queue* queue,Process_Data* proc) {
+    if (isEmpty(queue)) {
+        printf("Queue is empty, nothing to peek\n");
+        return;
+    }
+        proc = &queue->front->data;
+}
+
+void displayQueue(Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("Queue is empty\n");
+        return;
+    }
+    Node* current = queue->front;
+    printf("Queue elements: ");
+    while (current != NULL) {
+        displayProcessData(current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+
+
